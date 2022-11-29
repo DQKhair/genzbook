56c3f6dc92f33d6f056a3577ce18c326cedf0cc4
CodeXplore <jonddemon2307@gmail.com>
2022-11-29 21:44:25 +0700

    version 0.2

--- js/cart.js
+++ js/cart.js
@@ -1,1 +1,12 @@
 var chuyenthanhtoan = ()=> window.location.href="../donhang.html";
+var note_detail_voucher1 = document.getElementById('detail_form_voucher1');
+var note_detail_voucher2 = document.getElementById('detail_form_voucher2');
+function detail_voucher1(){
+    note_detail_voucher1.style.display='block';
+}
+function off_detail_voucher1(){
+    note_detail_voucher1.style.display='none';
+}
+
+var detail_voucher2 =()=> note_detail_voucher2.style.display='block'
+var off_detail_voucher2 =()=> note_detail_voucher2.style.display='none'
